388190abfc47720b2d282ace0cc39883753be0ff
AlexGarkaviy <vox01@mail.ru>
2017-01-14 22:52:38 +0200

    добавил бреда в  цикл

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,18 @@ public class Main {
     public static void main(String[]args){
         int i;
         int z ;
+
         Rog one = new Rog();
-        one.z= 1;
+        one.z= 0;
         Rog.sl(5,3);
         Rog.sl (2,8);
         Rog.sl (3,2);
+        one.setZ(3);
 
-        System.out.println(one.sl(2,9));
+        if (one.getZ() > 0) {
+            System.out.println(one.sl(2, 8));
+        }else
+        System.out.println(one.getZ());
 
     }
 }
--- src/com/company/Rog.java
+++ src/com/company/Rog.java
@@ -4,7 +4,15 @@ package com.company;
  * Created by garkaviy on 14.01.2017.
  */
 public class Rog {
-    public int z;
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public static int z;
 
     public static  int sl (int a , int b)
     {
