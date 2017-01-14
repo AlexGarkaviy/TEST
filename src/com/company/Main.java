a571611cb7995544633905f5854c4d2b286af06f
AlexGarkaviy <vox01@mail.ru>
2017-01-14 22:43:20 +0200

    Test

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+//public class Main {
+
+//public static void main(String[] args) {
+public class Main {
+
+    public static void main(String[]args){
+        int i;
+        int z ;
+        Rog one = new Rog();
+        one.z= 1;
+        Rog.sl(5,3);
+        Rog.sl (2,8);
+        Rog.sl (3,2);
+
+        System.out.println(one.sl(2,9));
+
+    }
+}
---
+++ src/com/company/Rog.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by garkaviy on 14.01.2017.
+ */
+public class Rog {
+    public int z;
+
+    public static  int sl (int a , int b)
+    {
+        return a-b;
+    }
+}
+
